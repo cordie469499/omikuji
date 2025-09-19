56d4ba8d8dc4e2bad5593812e2982ae558860095
cordie469499 <koudai06182003@gmail.com>
2025-09-19 20:39:58 +0900

    Update omikuji.js

--- omikuji.js
+++ omikuji.js
@@ -1,11 +1,11 @@
 function getRandomImage() {
-   const number = Math.floor(Math.random() * 7);
-   const imagePath = "./images/omikuji-" + number.toString() + ".png";
-   return imagePath;
+  const number = Math.floor(Math.random() * 7); // 0〜6まで
+  const imagePath = "images/omikuji-" + number.toString() + ".png";
+  return imagePath;
 }
 
 function playOmikuji() {
-    const slotAnimationTimer = setInterval(function () {
+  const slotAnimationTimer = setInterval(function () {
     document.querySelector("#js-result").setAttribute("src", getRandomImage());
   }, 100);
 
